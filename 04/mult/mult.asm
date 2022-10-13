0a619ebe0e00be467f6334c13100c120ab07b09d
peiyun328 <99934914+peiyun328@users.noreply.github.com>
2022-10-13 21:53:40 +0800

    Update mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,45 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+//a = R0
+//b = R1
+//R2 = 0
+//while(a > 0){
+//  R2 = R2 + b
+//  a --}
+
+@R0
+D=M
+@a
+M=D //a=R0
+
+@R1
+D=M
+@b
+M=D //b=R1
+
+@0
+D=A
+@R2
+M=D //R2=0
+
+
+@a
+D=M //load R0
+@END
+D;JLE //if a<=0 goto END      
+
+@b
+D=M //D=b
+@R2
+M=D+M //sum=sum+b
+@1
+D=A //D=1
+@a
+M=M-D //a=a-1    
+
+@LOOP
+0;JMP //jump to loop start
+
+@END
+0;JMP //program end
